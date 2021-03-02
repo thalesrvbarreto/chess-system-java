3c8f28c10ab5988ccfbe8676402c8cefa102b43b
Thales <thales.raphael@hotmail.com>
2021-03-01 21:06:45 -0300

    First class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("The Queen's Gambit!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
