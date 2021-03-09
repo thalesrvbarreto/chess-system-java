959714375161deabc8f55db91e41c61ac617efb9
unknown <thales.raphael@hotmail.com>
2021-03-09 16:56:21 -0300

    Moving Pieces

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Error instantiating chess position. Valid values are from A1 to A8");
+			throw new ChessException("Error instantiating chess position. Valid values are from A1 to H8");
 		}
 		this.column = column;
 		this.row = row;
